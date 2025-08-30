1695cf681eaeebe4acf09df8a873ef91d7617479
mdtamim1001 <mdtamim26301@gmail.com>
2025-08-30 11:19:14 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(cors());
 app.use(
   cors({
-    origin: ['*', 'http://localhost:5173'],
+    origin: ['*', 'http://localhost:5173','https://darling-panda-d34576.netlify.app'],
     methods: 'GET,POST,PUT,PATCH,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
