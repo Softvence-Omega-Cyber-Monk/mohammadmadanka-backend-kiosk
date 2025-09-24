3f4ee9c4eaa167a951d85c212496d1f7ac21a5b4
mdtamim1001 <mdtamim26301@gmail.com>
2025-09-24 17:44:34 +0600

    for deploy5

--- src/modules/occasion/occasion.interface.ts
+++ src/modules/occasion/occasion.interface.ts
@@ -2,6 +2,7 @@ export type Occasion = {
         name: string;
         iconUrl: string;
         public_id: string;
+        serialNumber: number;
         isDeleted: boolean;
         createdAt?: Date;
         updatedAt?: Date;
